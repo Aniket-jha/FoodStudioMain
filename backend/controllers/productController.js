586afebaadf22820815a10bccbf0bf7c0e86e065
Food studio <foodstudiofficial@gmail.com>
2023-03-13 01:48:23 +0530

    first commit

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,7 +8,6 @@ const cloudinary = require("cloudinary");
 
 
 exports.createProduct= catchAsyncErrors(async (req,res,next)=>{
-  let coverImages = []
     let images = [];
 
   if (typeof req.body.images === "string") {
@@ -16,15 +15,9 @@ exports.createProduct= catchAsyncErrors(async (req,res,next)=>{
   } else {
     images = req.body.images;
   }
-  if (typeof req.body.coverImages === "string") {
-    coverImages.push(req.body.coverImages);
-  } else {
-    coverImages = req.body.coverImages;
-  }
 
 
   const imagesLinks = [];
-  const coverImagesLinks = [];
 
   for (let i = 0; i < images.length; i++) {
     const result = await cloudinary.v2.uploader.upload(images[i], {
@@ -36,19 +29,8 @@ exports.createProduct= catchAsyncErrors(async (req,res,next)=>{
       url: result.secure_url,
     });
   }
-  for (let i = 0; i < coverImages.length; i++) {
-    const result = await cloudinary.v2.uploader.upload(coverImages[i], {
-      folder: "coverImages",
-    });
-
-    coverImagesLinks.push({
-      public_id: result.public_id,
-      url: result.secure_url,
-    });
-  }
 
   req.body.images = imagesLinks;
-  req.body.coverImages = coverImagesLinks
   
   req.body.user = req.user.id
     const product = await Product.create(req.body);
@@ -65,7 +47,7 @@ exports.createProduct= catchAsyncErrors(async (req,res,next)=>{
 
 exports.getAllProducts= catchAsyncErrors(async (req,res,next)=>{
   
-    const resultPerPage = 50;
+    const resultPerPage = 8;
     const productCount = await Product.countDocuments()
    const apiFeatures = new ApiFeatures(Product.find(), req.query).search().filter().pagination(resultPerPage)
    const products = await apiFeatures.query;
